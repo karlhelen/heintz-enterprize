ce791c31c3b176cbfe33ead6cf0f1fe8883eb951
karlhelen <karlhelen488@gmail.com>
2025-03-19 21:04:42 +0200

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -1,8 +1,22 @@
+import redirect from "next/navigation";
+import { login } from "../../../lib/actions.js";
+
 export default function Login() {
 
   return (
     <main>
-
+      <h1>Login</h1>
+      <div>
+        <form action={async (formData) => {
+          "use server";
+          await login(formData);
+          redirect('/dashboard');
+        }}>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </main>
   )
 }
