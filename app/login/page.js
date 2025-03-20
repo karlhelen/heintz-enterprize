c69b2756c561eb044fa7808e87f3fbf89fb2a0ad
karlhelen <karlhelen488@gmail.com>
2025-03-20 08:53:32 +0000

    updated login page, sign up page

--- app/login/page.js
+++ app/login/page.js
@@ -15,11 +15,7 @@ export default function Login() {
     <main>
       <h1>Login</h1>
       <div>
-        <form action={async (formData) => {
-          "use server";
-          await login(formData);
-          redirect('/dashboard');
-        }}>
+        <form action={login}>
           <input type="email" name="email" value={user.email} onChange={(e) => setUser.email(e.target.value)} placeholder="Email" />
           <input type="password" name="password" value={user.password} onChange={(e) => setUser.password(e.target.value)} placeholder="Password" />
           <button type="submit">Login</button>
